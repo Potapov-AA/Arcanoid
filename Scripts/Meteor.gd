b9c2a5aa9af4d6b535e3c03af9bb0242d1ad08b4
JESTER <jestterr@mail.ru>
2023-01-21 17:40:07 +0300

    Обновил механики
    Добавил механику размера метеорита

--- Scripts/Meteor.gd
+++ Scripts/Meteor.gd
@@ -1,13 +1,32 @@
 extends KinematicBody2D
 
-export (int) var speed = 2
+export (float) var speed = 1
 
-var health = 100
+var health = 50
+var score = 10
 var velocity = Vector2()
 
-signal no_visibility
-signal destroy
 
+func _ready():
+	randomize()
+	var coefficient = int(rand_range(1, 7))
+	health *= coefficient
+	score *= coefficient
+
+
+func _process(delta):
+	if health == 250 or health == 300:
+		$AnimatedSprite.play("big_meteor")
+		speed = 1
+		$CollisionShape2D.scale = Vector2(2, 2)
+	if health == 150 or health == 200:
+		$AnimatedSprite.play("middle_meteor")
+		speed = 1.5
+		$CollisionShape2D.scale = Vector2(1.5, 1.5)
+	if health == 50 or health == 100:
+		$AnimatedSprite.play("small_meteor")
+		speed = 2
+		$CollisionShape2D.scale = Vector2(1, 1)
 
 func _physics_process(delta):
 	velocity = Vector2(0, -speed).rotated(rotation)
@@ -20,7 +39,7 @@ func _physics_process(delta):
 			queue_free()
 	
 	if health <= 0 and health != -1:
-		get_parent().update_score(10)
+		get_parent().update_score(score)
 		queue_free()
 		
 
